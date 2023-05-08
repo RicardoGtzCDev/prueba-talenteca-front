4142614c654cbd975c75b3ae2a527d9cdd545667
Ricardo Gutiérrez Cañas <ricardo.gtzc.dev@gmail.com>
2023-05-08 17:04:34 -0600

    correción de .env para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrls: {
     local: 'https://localhost:7230/api',
-    back: 'https://talenteca-back.azurewebsites.net/api',
+    azure: 'https://talenteca-back.azurewebsites.net/api',
   },
 };
